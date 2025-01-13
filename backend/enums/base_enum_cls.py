41b4c42fa86ec1aa75ff27b5d548e38f021a13e8
KKNOBUG <807440781@qq.com>
2025-01-13 09:13:01 +0800

    1.实现公共枚举值类封装和基础错误枚举

--- backend/enums/base_enum_cls.py
+++ backend/enums/base_enum_cls.py
@@ -7,17 +7,27 @@
 @DateTime: 2025/1/12 22:58
 """
 from enum import Enum
+from typing import cast
 
 
 class BaseEnumCls(Enum):
+    """
+    基础枚举类，为自定义枚举提供通用的功能。
+
+    该类继承自Python内置的Enum类，通过重写__new__方法，为枚举成员添加描述信息，并提供类一系列类方法。
+    用于获取枚举成员的各种信息。
+    """
 
     def __new__(cls, value, desc=None, *args, **kwargs):
         """
+        创建并返回一个新的枚举成员实例。
+        此方法重写类Enum类的__new__方法，用于为枚举成员添加自定义的描述信息。
 
-        :param value:
-        :param desc:
-        :param args:
-        :param kwargs:
+        :param value: 枚举成员的值，可以是任意类型，具体取决于枚举类的类型（如str或int）。
+        :param desc: 枚举成员的描述信息，默认为None。
+        :param args: 位置参数。
+        :param kwargs: 关键字参数。
+        :return: 新创建的枚举成员实例。
         """
 
         if issubclass(cls, int):
@@ -35,11 +45,12 @@ class BaseEnumCls(Enum):
     def get_members(cls, exclude_enums: list = None,
                     only_value: bool = False, only_desc: bool = False) -> list:
         """
-        获取枚举的所有成员
-        :param exclude_enums:
-        :param only_value:
-        :param only_desc:
-        :return:
+        获取枚举的所有成员，可以根据条件进行筛选和转换。
+
+        :param exclude_enums: 要排除的枚举成员列表，默认为None。
+        :param only_value: 是否只返回枚举成员的值，默认为False。
+        :param only_desc: 是否只返回枚举成员的描述信息，默认为False。
+        :return: 符合条件的枚举成员列表。
         """
 
         members = list(cls)
@@ -58,19 +69,42 @@ class BaseEnumCls(Enum):
         return members
 
     @classmethod
-    def get_names(cls):
+    def get_names(cls) -> list:
+        """
+        获取枚举的所有成员名称。
+        :return: 包含所有枚举成员名称的列表
+        """
         return list(cls._member_names_)
 
     @classmethod
     def get_values(cls, exclude_enums: list = None):
+        """
+        获取枚举的所有成员值，可以排除指定的枚举成员
+
+        :param exclude_enums: 要排除的枚举成员列表，默认为None。
+        :return: 包含所有枚举成员值的列表。
+        """
         return cls.get_members(exclude_enums=exclude_enums, only_value=True)
 
     @classmethod
     def get_desc(cls, exclude_enums: list = None):
+        """
+        获取枚举的所有成员描述信息，可以排除指定的枚举成员
+
+        :param exclude_enums: 要排除的枚举成员列表，默认为None。
+        :return: 包含所有枚举成员值的列表。
+        """
         return cls.get_members(exclude_enums=exclude_enums, only_desc=True)
 
     @classmethod
     def get_member_by_desc(cls, enum_desc, only_value: bool = False):
+        """
+        根据描述信息获取对应的枚举成员
+
+        :param enum_desc: 要查找的枚举成员的描述信息
+        :param only_value:  是否只返回枚举成员的值，默认为False。
+        :return: 对应的枚举成员或其值（根据only_value参数觉得）。
+        """
         members = cls.get_members()
         members_dict = {m.desc: m for m in members}
         member = members_dict.get(enum_desc)
@@ -78,10 +112,16 @@ class BaseEnumCls(Enum):
 
 
 class StringEnum(str, BaseEnumCls):
+    """
+    字符串类型的枚举类，继承自BaseEnumCls，用于创建字符串类型的枚举
+    """
     pass
 
 
 class IntegerEnum(int, BaseEnumCls):
+    """
+    整数类型的枚举类，继承自BaseEnumCls，用于创建整数类型的枚举
+    """
     pass
 
 
