b71217e2d87bf85c39dc3dbb3361e7d09bb52334
KKNOBUG <807440781@qq.com>
2025-01-27 10:30:54 +0800

    1.新增api接口应用

---
+++ backend/applications/base/models/api_model.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : yangkai
+@Email   : 807440781@qq.com
+@Project : Krun
+@Module  : api_model.py
+@DateTime: 2025/1/27 10:04
+"""
+from tortoise import fields
+
+from backend.enums.http_enum import HTTPMethod
+from backend.services.base_model import BaseModel, TimestampMixin
+
+
+class Api(BaseModel, TimestampMixin):
+    path = fields.CharField(max_length=128, description="API路径", index=True)
+    method = fields.CharEnumField(HTTPMethod, description="请求方法", index=True)
+    summary = fields.CharField(max_length=500, description="请求简介", index=True)
+    tags = fields.CharField(max_length=100, description="API标签", index=True)
+
+    class Meta:
+        table = "krun_api"
---
+++ backend/applications/base/views/api_view.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Author  : yangkai
+@Email   : 807440781@qq.com
+@Project : Krun
+@Module  : api_view.py
+@DateTime: 2025/1/27 10:15
+"""
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('Python')
