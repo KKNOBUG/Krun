d7f806996aba2755955ac265aa61c01a4bafa1bd
KKNOBUG <807440781@qq.com>
2025-02-20 20:05:38 +0800

    1.优化日志配置

--- backend/configure/logging_config.py
+++ backend/configure/logging_config.py
@@ -9,8 +9,18 @@
 import logging.config
 from datetime import datetime
 from typing import Dict, Any
+
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+
 from backend import PROJECT_CONFIG
 
+
+def get_log_filename(log_type: str) -> str:
+    """生成动态日志文件名"""
+    return f'{PROJECT_CONFIG.OUTPUT_LOGS_DIR}/{datetime.now().strftime("%Y%m%d")}_{log_type}_执行日志.log'
+
+
+# 可以使用 ConcurrentLogHandler 来处理多进程环境中的日志记录。这个处理器可以确保在多进程环境中安全地写入日志文件。
 DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -38,8 +48,10 @@ DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
         },
         'production_file': {
             'level': 'INFO',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': f'{PROJECT_CONFIG.OUTPUT_LOGS_DIR}/{datetime.now().strftime("%Y%m%d")}_INFO_执行日志.log',
+            # 'class': 'logging.handlers.RotatingFileHandler',
+            'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+            # 'filename': f'{PROJECT_CONFIG.OUTPUT_LOGS_DIR}/{datetime.now().strftime("%Y%m%d")}_INFO_执行日志.log',
+            'filename': get_log_filename("INFO"),  # 使用动态文件名
             'maxBytes': 1024 * 1024 * 10,
             'backupCount': 10,
             'formatter': 'main_formatter',
@@ -47,8 +59,10 @@ DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
         },
         'debug_file': {
             'level': 'DEBUG',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': f'{PROJECT_CONFIG.OUTPUT_LOGS_DIR}/{datetime.now().strftime("%Y%m%d")}_DEBUG_执行日志.log',
+            # 'class': 'logging.handlers.RotatingFileHandler',
+            'class': 'concurrent_log_handler.ConcurrentRotatingFileHandler',
+            # 'filename': f'{PROJECT_CONFIG.OUTPUT_LOGS_DIR}/{datetime.now().strftime("%Y%m%d")}_DEBUG_执行日志.log',
+            'filename': get_log_filename("DEBUG"),  # 使用动态文件名
             'maxBytes': 1024 * 1024 * 10,
             'backupCount': 10,
             'formatter': 'main_formatter',
@@ -59,6 +73,7 @@ DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
         '': {
             'handlers': ['console', 'production_file', 'debug_file'],
             'level': "DEBUG",
+            'propagate': True,
         },
 
     }
@@ -67,9 +82,9 @@ DEFAULT_LOGGING_CONFIG: Dict[str, Any] = {
 
 class RequireDebugFalse(logging.Filter):
     def filter(self, record):
-        return not PROJECT_CONFIG.SERVER_DEBUG
+        return not getattr(PROJECT_CONFIG, 'SERVER_DEBUG', True)
 
 
 class RequireDebugTrue(logging.Filter):
     def filter(self, record):
-        return PROJECT_CONFIG.SERVER_DEBUG
+        return getattr(PROJECT_CONFIG, 'SERVER_DEBUG', False)
