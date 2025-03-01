1f0e383b2bd133ccde1498133fb94acbee18e599
KKNOBUG <807440781@qq.com>
2025-03-01 18:18:56 +0800

    1.工具箱-虚拟数据生成页面

--- frontend/src/views/toolbox/generate/index.vue
+++ frontend/src/views/toolbox/generate/index.vue
@@ -1,70 +1,77 @@
 <template>
   <AppPage>
     <n-card style="margin-bottom: 16px">
-      <n-tabs default-value="person" justify-content="space-evenly" type="line">
-        <n-tab-pane name="person" tab="人员信息">
-          <n-form
-              ref="formRef"
-              :model="model"
-              :rules="rules"
-              label-placement="left"
-              label-width="auto"
-              require-mark-placement="right-hanging"
-              size="medium"
-              :style="{ maxWidth: '650px' }"
-          >
-            <n-form-item label="生成数量：" path="number">
-              <n-input-number v-model:value="model.number" clearable min="1"/>
-            </n-form-item>
-            <n-form-item label="生成年龄：" path="age">
-              <n-input pair separator="-" type="number" clearable
-                       :placeholder="placeholder" v-model:value="model.age"/>
-            </n-form-item>
-            <n-form-item label="生成选择：" path="option">
-              <n-transfer v-model:value="model.option" :options="generalOptions"/>
-            </n-form-item>
-            <n-button type="primary" size="large" @click="generateBtn">
-              生成数据
-            </n-button>
-          </n-form>
-          <!-- 结果展示区域 -->
-          <n-divider/>
-          <div v-if="showResult" class="result-container">
-            <n-card>
-              <div style="display: flex; justify-content: flex-end; margin-bottom: 8px;">
-                <n-button @click="copyResult">复制结果</n-button>
-              </div>
-              <pre ref="jsonPre">{{ JSON.stringify(resultData, null, 4) }}</pre>
-            </n-card>
+      <n-form
+          ref="formRef"
+          :model="model"
+          :rules="rules"
+          label-placement="left"
+          label-width="auto"
+          require-mark-placement="right-hanging"
+          size="medium"
+          :style="{ maxWidth: '650px' }"
+      >
+        <n-divider title-placement="left">
+          个人信息
+        </n-divider>
+        <n-form-item label="数量：" path="number">
+          <n-input-number v-model:value="model.number" clearable min="1"/>
+        </n-form-item>
+        <n-form-item label="年龄：" path="age">
+          <n-input pair separator="-" type="number" clearable
+                   :placeholder="placeholder" v-model:value="model.age"/>
+        </n-form-item>
+        <n-form-item label="选项：" path="personOption" class="transfer-container">
+          <n-transfer v-model:value="model.personOption" :options="generatePersonOptions"/>
+        </n-form-item>
+
+        <n-divider title-placement="left">
+          时间信息
+        </n-divider>
+        <n-form-item label="选项：" path="datetimeOption" class="transfer-container">
+          <n-transfer v-model:value="model.datetimeOption" :options="generateDatetimeOptions"/>
+        </n-form-item>
+        <n-divider title-placement="left">
+          随机数字
+        </n-divider>
+        <n-form-item label="选项：" path="randomOption" class="transfer-container">
+          <n-transfer v-model:value="model.randomOption" :options="generateRandomOptions"/>
+        </n-form-item>
+        <n-button type="primary" size="large" @click="generateBtn">
+          生成数据
+        </n-button>
+      </n-form>
+      <!-- 结果展示区域 -->
+      <n-divider/>
+      <div v-if="showResult" class="result-container">
+        <n-card>
+          <div style="display: flex; justify-content: flex-end; margin-bottom: 8px;">
+            <n-button @click="copyResult">复制结果</n-button>
           </div>
-        </n-tab-pane>
-        <n-tab-pane name="datetime" tab="时间日期">
-          时间日期2
-        </n-tab-pane>
-        <n-tab-pane name="jay chou" tab="随机数">
-          随机数3
-        </n-tab-pane>
-      </n-tabs>
+          <pre ref="jsonPre">{{ JSON.stringify(resultData, null, 4) }}</pre>
+        </n-card>
+      </div>
     </n-card>
   </AppPage>
 </template>
 
 <script>
 import {defineComponent, ref} from "vue";
-import { useMessage } from "naive-ui";
+import {useMessage} from "naive-ui";
+import api from "@/api";
 import hljs from "highlight.js";
 import "highlight.js/styles/default.css";
-import api from "@/api";
+import AppPage from "@/components/page/AppPage.vue";
 
 export default defineComponent({
+  components: {AppPage},
   setup() {
     const formRef = ref(null);
-    const model = ref({number: 1, age: null, option: null})
+    const model = ref({number: 1, age: null, personOption: null, datetimeOption: null, randomOption: null})
     const showResult = ref(false);
     const resultData = ref([]);
     const jsonPre = ref(null);
     const message = useMessage();
-
 
     // 验证输入的年龄区间
     const inputAgeRangeValid = (rule, value, callback) => {
@@ -95,7 +102,29 @@ export default defineComponent({
         validator: inputAgeRangeValid,
         trigger: ["blur", "change"],
       },
-      option: {
+      personOption: {
+        required: true,
+        trigger: ["blur", "change"],
+        validator: (rule, value, callback) => {
+          if (!value || value.length === 0) {
+            callback(new Error("请选择生成选项"));
+          } else {
+            callback();
+          }
+        }
+      },
+      datetimeOption: {
+        required: true,
+        trigger: ["blur", "change"],
+        validator: (rule, value, callback) => {
+          if (!value || value.length === 0) {
+            callback(new Error("请选择生成选项"));
+          } else {
+            callback();
+          }
+        }
+      },
+      randomOption: {
         required: true,
         trigger: ["blur", "change"],
         validator: (rule, value, callback) => {
@@ -111,7 +140,7 @@ export default defineComponent({
     const generateBtn = async () => {
       try {
         // 解构 age 列表为 minAge 和 maxAge
-        const { age, ...rest } = model.value;
+        const {age, ...rest} = model.value;
         const [minAge, maxAge] = age || [];
         const requestData = {
           ...rest,
@@ -168,7 +197,7 @@ export default defineComponent({
       generateBtn,
       size: ref("small"),
       placeholder: ["最小年龄", "最大年龄"],
-      generalOptions: [
+      generatePersonOptions: [
         "中文姓名",
         "英文姓名",
         "年龄",
@@ -187,6 +216,23 @@ export default defineComponent({
         label: v,
         value: v
       })),
+      generateDatetimeOptions: [
+        "现在",
+        "历史",
+        "未来"
+      ].map((v) => ({
+        label: v,
+        value: v
+      })),
+      generateRandomOptions: [
+        "UUID",
+        "时间戳",
+        "流水号",
+        "随机数"
+      ].map((v) => ({
+        label: v,
+        value: v
+      })),
       showResult,
       resultData,
       highlightJson,
@@ -197,3 +243,10 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+:deep(.n-transfer) {
+  height: 180px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
