ea832ce9a36fea31c0b0a166faaef2fd4072ee07
KKNOBUG <807440781@qq.com>
2025-03-01 18:17:50 +0800

    1.虚拟数据生成接口实现

--- backend/applications/toolbox/views/generate_view.py
+++ backend/applications/toolbox/views/generate_view.py
@@ -6,26 +6,105 @@
 @Module  : generate_view.py
 @DateTime: 2025/2/28 14:43
 """
-from typing import List
+from typing import List, Dict, Any
 
 from fastapi import APIRouter, Body
 
-from backend.applications.toolbox.schemas.generate_schema import GeneratePerson
+from backend.applications.toolbox.schemas.generate_schema import GenerateVirtualInfo
 from backend.core.response.http_response import SuccessResponse
 from backend import GENERATE
 
 generate = APIRouter()
 
 
-@generate.post("/person", summary="生成虚拟人员信息")
-async def generate_person(req_in: GeneratePerson = Body(...)):
-    data: List[dict] = []
-    number: int = req_in.number
-    minAge: int = req_in.minAge
-    maxAge: int = req_in.maxAge
-    option: List[str] = req_in.option
-    for i in range(number):
+@generate.post("/person", summary="随机生成虚拟信息")
+async def generate_person(rq_in: GenerateVirtualInfo = Body(...)):
+    data: Dict[str, Any] = {}
+    number: int = rq_in.number
+    minAge: int = rq_in.minAge
+    maxAge: int = rq_in.maxAge
+    personOption: List[str] = rq_in.personOption
+    datetimeOption: List[str] = rq_in.datetimeOption
+    randomOption: List[str] = rq_in.randomOption
+
+    # 随机人员
+    for x in range(number):
         info = GENERATE.generate_information(minAge=minAge, maxAge=maxAge, convert="capitalize")
-        data.append({k: v for k, v in info.items() if k in option})
+        data.setdefault("person", []).append({k: v for k, v in info.items() if k in personOption})
+
+    # 随机时间
+    formats: Dict[int, str] = {
+        11: "Y",
+        12: "M",
+        13: "D",
+        14: "H",
+        15: "M",
+        16: "S",
+
+        21: "YMD1",
+        22: "YMD2",
+        23: "YMD3",
+
+        31: "HMS1",
+        32: "HMS2",
+        33: "HMS3",
+
+        41: "YMDHMS1",
+        42: "YMDHMS2",
+        43: "YMDHMS3",
+
+        51: "YMDHMSF1",
+        52: "YMDHMSF2",
+        53: "YMDHMSF3",
+    }
+    for y in datetimeOption:
+        if y == "now":
+            for key, value in formats.items():
+                item = GENERATE.generate_datetime(fmt=key, isMicrosecond=key > 50)
+                data.setdefault(y, {}).setdefault(value, item)
+        elif y == "history":
+            year = GENERATE.generate_random_number(-50, -10)
+            month = GENERATE.generate_random_number(-12, -1)
+            day = GENERATE.generate_random_number(-31, -1)
+            hour = GENERATE.generate_random_number(-60, -1)
+            minute = GENERATE.generate_random_number(-60, -1)
+            second = GENERATE.generate_random_number(-60, -1)
+            for key, value in formats.items():
+                item = GENERATE.generate_datetime(
+                    year=year, month=month, day=day,
+                    hour=hour, minute=minute, second=second,
+                    fmt=key, isMicrosecond=key > 50)
+                data.setdefault(y, {}).setdefault(value, item)
+        elif y == "future":
+            year = GENERATE.generate_random_number(5, 50)
+            month = GENERATE.generate_random_number(1, 12)
+            day = GENERATE.generate_random_number(1, 31)
+            hour = GENERATE.generate_random_number(1, 60)
+            minute = GENERATE.generate_random_number(1, 60)
+            second = GENERATE.generate_random_number(1, 60)
+            for key, value in formats.items():
+                item = GENERATE.generate_datetime(
+                    year=year, month=month, day=day,
+                    hour=hour, minute=minute, second=second,
+                    fmt=key, isMicrosecond=key > 50)
+                data.setdefault(y, {}).setdefault(value, item)
+
+    # 随机数字
+    for z in randomOption:
+        if z == "uuid":
+            number: int = GENERATE.generate_random_number(3, 10)
+            data[z] = [GENERATE.generate_uuid for _ in range(number)]
+        elif z == "timestamp":
+            data[z] = GENERATE.generate_timestamp
+        elif z == "global":
+            data[z] = GENERATE.generate_global_serial_number
+        elif z == "random":
+            series: list = []
+            number: int = GENERATE.generate_random_number(3, 10)
+            for i in range(number):
+                length: int = GENERATE.generate_random_number(8, 20)
+                series.append(GENERATE.generate_string(length=length))
+
+            data[z] = series
 
     return SuccessResponse(data=data)
