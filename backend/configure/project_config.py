a4fbba7eca1e485e947ee43275f597a02137d1f3
KKNOBUG <807440781@qq.com>
2025-03-05 14:16:46 +0800

    1.更新项目配置

--- backend/configure/project_config.py
+++ backend/configure/project_config.py
@@ -7,37 +7,41 @@
 @DateTime: 2025/1/15 16:08
 """
 import os.path
-from pathlib import Path
+import platform
+from typing import List, Dict, Any
+
+from pydantic_settings import BaseSettings
 
 from backend.common.file_utils import FileUtils
 from backend.common.shell_utils import ShellUtils
 
 
-class ProjectConfig:
+class ProjectConfig(BaseSettings):
     # 项目描述
-    APP_VERSION = "0.1.1"
-    APP_TITLE = "KRUN - 测管平台"
-    APP_DESCRIPTION = """
+    APP_VERSION: str = "0.1.1"
+    APP_TITLE: str = "KRUN - 测管平台"
+    APP_DESCRIPTION: str = """
     KRUN 测管平台是一款基于 Python 的 FastAPI 框架开发的实用型测试管理系统，旨在满足软件测试工作的日常需求。
     它专注于提供最实用的功能，涵盖测试用例管理、测试环境配置、测试任务调度以及测试报告生成等多个关键环节。
     用户可以轻松导入和导出测试用例，灵活调整测试计划，根据不同的测试阶段和项目需求对测试任务进行精确管理。
     凭借 FastAPI 的高效性能，平台能够迅速响应各种操作，确保测试工作的连贯性和高效性。
     同时，系统还支持历史测试数据的回溯和对比，帮助团队持续优化测试流程，为软件质量的提升提供强大支持。
     """
-    APP_DOCS_URL = "/krun/docs"
-    APP_REDOC_URL = "/krun/redoc"
-    APP_OPENAPI_URL = "/krun/openapi_url"
-    APP_OPENAPI_JS_URL = "/static/swagger-ui/swagger-ui-bundle.js"
-    APP_OPENAPI_CSS_URL = "/static/swagger-ui/swagger-ui.css"
-    APP_OPENAPI_FAVICON_URL = "/static/swagger-ui/favicon-32x32.png"
-    APP_OPENAPI_VERSION = "3.0.2"
+    APP_DOCS_URL: str = "/krun/docs"
+    APP_REDOC_URL: str = "/krun/redoc"
+    APP_OPENAPI_URL: str = "/krun/openapi_url"
+    APP_OPENAPI_JS_URL: str = "/static/swagger-ui/swagger-ui-bundle.js"
+    APP_OPENAPI_CSS_URL: str = "/static/swagger-ui/swagger-ui.css"
+    APP_OPENAPI_FAVICON_URL: str = "/static/swagger-ui/favicon-32x32.png"
+    APP_OPENAPI_VERSION: str = "3.0.2"
 
     # 调试配置
-    SERVER_APP = "backend_main:app"
-    SERVER_HOST = ShellUtils.acquire_localhost()
-    SERVER_PORT = 8518
-    SERVER_DEBUG = True
-    SERVER_DELAY = 5
+    SERVER_APP: str = "backend_main:app"
+    SERVER_HOST: str = ShellUtils.acquire_localhost()
+    SERVER_SYSTEM: str = platform.system()
+    SERVER_PORT: int = 8518
+    SERVER_DEBUG: bool = SERVER_SYSTEM != "Linux"  # Windows | Linux | Darwin
+    SERVER_DELAY: int = 5
 
     # 安全认证配置
     AUTH_SECRET_KEY: str = "3488a63e1765035d386f05409663f55c83bfae3b3c61a932744b20ad14244dcf"  # openssl rand -hex 32
@@ -46,38 +50,44 @@ class ProjectConfig:
 
     # 日志相关参数配置
     # 文件名称前缀
-    LOGGER_FILE_NAME_PREFIX = "执行日志"
+    LOGGER_FILE_NAME_PREFIX: str = "执行日志"
     # 是否开启文件名称后缀（是否增加年月日标识）
-    LOGGER_FILE_NAME_SUFFIX = True
+    LOGGER_FILE_NAME_SUFFIX: bool = True
     # 日志文件轮转频率
-    LOGGER_TIMED_ROTATING = "天"
+    LOGGER_TIMED_ROTATING: str = "天"
     # 保留的备份日志文件的数量，当超过超过这个数量时，最旧的备份文件将被删除，默认值为0，标识不保留备份
-    LOGGER_BACKUP_COUNT = 0
+    LOGGER_BACKUP_COUNT: int = 0
+    # 文件大小轮转
+    LOGGER_ROTATION: str = '10 MB'
+    # 保留30天
+    LOGGER_RETENTION: str = '30 days'
+    # 压缩格式
+    LOGGER_COMPRESSION: str = "zip"
 
     # 项目路径相关配置
-    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-    APPLICATIONS_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "applications"))
-    COMMON_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "common"))
-    CONFIGURE_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "configure"))
-    CORE_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "core"))
-    DECORATORS_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "decorators"))
-    ENUMS_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "enums"))
-    OUTPUT_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "output"))
-    OUTPUT_LOGS_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "logs"))
-    OUTPUT_UPLOAD_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "upload"))
-    OUTPUT_DOWNLOAD_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "download"))
-    OUTPUT_MEDIA_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "media"))
-    OUTPUT_DATAGRAM_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "datagram"))
-    OUTPUT_JMX_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "jmx"))
-    OUTPUT_XLSX_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "xlsx"))
-    OUTPUT_DOCS_DIR = os.path.abspath(os.path.join(OUTPUT_DIR, "docs"))
-    SERVICES_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "services"))
-    STATIC_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "static"))
-    STATIC_IMG_DIR = os.path.abspath(os.path.join(STATIC_DIR, "image"))
-    MIGRATION_DIR = os.path.abspath(os.path.join(PROJECT_ROOT, "migrations"))
+    PROJECT_ROOT: str = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+    APPLICATIONS_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "applications"))
+    COMMON_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "common"))
+    CONFIGURE_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "configure"))
+    CORE_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "core"))
+    DECORATORS_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "decorators"))
+    ENUMS_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "enums"))
+    OUTPUT_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "output"))
+    OUTPUT_LOGS_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "logs"))
+    OUTPUT_UPLOAD_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "upload"))
+    OUTPUT_DOWNLOAD_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "download"))
+    OUTPUT_MEDIA_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "media"))
+    OUTPUT_DATAGRAM_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "datagram"))
+    OUTPUT_JMX_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "jmx"))
+    OUTPUT_XLSX_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "xlsx"))
+    OUTPUT_DOCS_DIR: str = os.path.abspath(os.path.join(OUTPUT_DIR, "docs"))
+    SERVICES_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "services"))
+    STATIC_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "static"))
+    STATIC_IMG_DIR: str = os.path.abspath(os.path.join(STATIC_DIR, "image"))
+    MIGRATION_DIR: str = os.path.abspath(os.path.join(PROJECT_ROOT, "migrations"))
 
     # 跨域配置
-    CORS_ORIGINS = [
+    CORS_ORIGINS: List[str] = [
         "http://localhost",
         "http://localhost:5000",
         "http://localhost:5173",
@@ -85,15 +95,15 @@ class ProjectConfig:
         "http://localhost:8515",
         "*",
     ]
-    CORS_ALLOW_CREDENTIALS = True
-    CORS_ALLOW_METHODS = ["*"]
-    CORS_ALLOW_HEADERS = ["*"]
-    CORS_EXPOSE_METHODS = ["*"]
-    CORS_MAX_AGE = 600
+    CORS_ALLOW_CREDENTIALS: bool = True
+    CORS_ALLOW_METHODS: List[str] = ["*"]
+    CORS_ALLOW_HEADERS: List[str] = ["*"]
+    CORS_EXPOSE_METHODS: List[str] = ["*"]
+    CORS_MAX_AGE: int = 600
 
     # 应用注册
-    APPLICATIONS_MODULE = "backend.applications"
-    APPLICATIONS_INSTALLED = FileUtils.get_all_dirs(
+    APPLICATIONS_MODULE: str = "backend.applications"
+    APPLICATIONS_INSTALLED: List[str] = FileUtils.get_all_dirs(
         abspath=APPLICATIONS_DIR,
         return_full_path=False,
         exclude_startswith="__",
@@ -101,7 +111,7 @@ class ProjectConfig:
     )
 
     @property
-    def APPLICATIONS_MODELS(self):
+    def APPLICATIONS_MODELS(self) -> List[str]:
         models = [
             models
             for app in self.APPLICATIONS_INSTALLED
@@ -118,16 +128,16 @@ class ProjectConfig:
         return models
 
     # 数据库配置
-    # DATABASE_USERNAME = quote("admin@usr")
-    # DATABASE_PASSWORD = quote("admin@Pwd123")
-    DATABASE_USERNAME = "root"
-    DATABASE_PASSWORD = "root"
-    DATABASE_HOST = "10.211.55.3"
-    # DATABASE_HOST = "43.156.105.196"
-    DATABASE_PORT = "3306"
-    DATABASE_NAME = "krun"
-    DATABASE_URL = f"mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}?charset=utf8mb4&time_zone=+08:00"
-    DATABASE_CONNECTIONS = {
+    # DATABASE_USERNAME: str = quote("admin@usr")
+    # DATABASE_PASSWORD: str = quote("admin@Pwd123")
+    DATABASE_USERNAME: str = "root"
+    DATABASE_PASSWORD: str = "root"
+    DATABASE_HOST: str = "10.211.55.3"
+    # DATABASE_HOST: str = "43.156.105.196"
+    DATABASE_PORT: str = "3306"
+    DATABASE_NAME: str = "krun"
+    DATABASE_URL: str = f"mysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}?charset=utf8mb4&time_zone=+08:00"
+    DATABASE_CONNECTIONS: Dict[str, Any] = {
         "default": {
             "engine": "tortoise.backends.mysql",  # 使用mysql引擎
             "db_url": DATABASE_URL,
@@ -147,11 +157,11 @@ class ProjectConfig:
     }
 
     # Redis 配置
-    REDIS_USERNAME = ""
-    REDIS_PASSWORD = ""
-    REDIS_HOST = ""
-    REDIS_PORT = ""
-    REDIS_URL = f"redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0"
+    REDIS_USERNAME: str = ""
+    REDIS_PASSWORD: str = ""
+    REDIS_HOST: str = ""
+    REDIS_PORT: str = ""
+    REDIS_URL: str = f"redis://{REDIS_USERNAME}:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0"
 
 
 PROJECT_CONFIG = ProjectConfig()
