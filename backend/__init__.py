c3e0bf1a2e7132252b0393f8c909c4684231416b
KKNOBUG <807440781@qq.com>
2025-03-05 16:06:51 +0800

    1.移除未使用的导入

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,13 +6,10 @@
 @Module  : __init__.py.py
 @DateTime: 2025/1/12 19:38
 """
-import logging
-
 from backend.configure.project_config import PROJECT_CONFIG
 from backend.configure.global_config import GLOBAL_CONFIG
+from backend.configure.logging_config import logger as LOGGER
 from backend.common.generate_utils import GENERATE
-
-LOGGER = logging.getLogger(__name__)
 
 __all__ = (
     PROJECT_CONFIG,
