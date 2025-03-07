c1e194af042ff786e111f39367500792b2d018f0
KKNOBUG <807440781@qq.com>
2025-03-07 23:57:00 +0800

    1.实现审计日志

--- backend/applications/base/models/audit_model.py
+++ backend/applications/base/models/audit_model.py
@@ -9,17 +9,26 @@
 from tortoise import fields
 
 from backend.applications.base.services.scaffold import ScaffoldModel, MaintainMixin, TimestampMixin
+from backend.enums.http_enum import HTTPMethod
 
 
 class Audit(ScaffoldModel, TimestampMixin):
     user_id = fields.BigIntField(index=True, description="用户ID")
-    username = fields.CharField(max_length=32, default="", index=True, description="用户名称")
-    module = fields.CharField(max_length=64, default="", index=True, description="功能模块")
-    summary = fields.CharField(max_length=128, default="", index=True, description="请求描述")
-    method = fields.CharField(max_length=16, default="", index=True, description="请求方法")
-    path = fields.CharField(max_length=255, default="", index=True, description="请求路径")
-    state = fields.SmallIntField(default=-1, index=True, description="状态码")
-    response_time = fields.IntField(default=0, index=True, description="响应时间(单位ms)")
+    username = fields.CharField(max_length=32, index=True, description="用户名称")
+    request_time = fields.DatetimeField(description="请求时间")
+    request_tags = fields.CharField(max_length=255, default="", null=True, index=True, description="请求模块")
+    request_summary = fields.CharField(max_length=255, default="", null=True, index=True, description="请求接口")
+    request_method = fields.CharEnumField(HTTPMethod, index=True, description="请求方式")
+    request_router = fields.CharField(max_length=255, index=True, description="请求路由")
+    request_client = fields.CharField(max_length=16, default="", null=True, description="请求来源")
+    request_header = fields.JSONField(default=None, null=True, description="请求头部")
+    request_params = fields.TextField(default="", null=True, description="请求参数")
+    response_time = fields.DatetimeField(description="响应时间")
+    response_header = fields.JSONField(default=None, null=True, description="响应头部")
+    response_code = fields.CharField(max_length=16, default="", null=True, description="响应代码")
+    response_message = fields.CharField(max_length=255, default="", null=True, description="响应消息")
+    response_params = fields.TextField(default="", null=True, description="响应参数")
+    response_elapsed = fields.CharField(max_length=16, description="响应耗时")
 
     class Meta:
         table = "krun_audit"
