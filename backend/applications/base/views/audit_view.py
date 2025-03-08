6076e00be0710bdf2229a2e2e3d9c73a06d7d36e
KKNOBUG <807440781@qq.com>
2025-03-08 18:55:20 +0800

    1.实现审计日志

--- backend/applications/base/views/audit_view.py
+++ backend/applications/base/views/audit_view.py
@@ -19,25 +19,25 @@ audit = APIRouter()
 async def list_audit(
         page: int = Query(default=1, ge=1, description="页码"),
         page_size: int = Query(default=10, ge=10, description="每页数量"),
-        username: str = Query(default="", description="操作人名称"),
-        module: str = Query(default="", description="功能模块"),
-        method: str = Query(default="", description="请求方法"),
-        summary: str = Query(default="", description="接口描述"),
-        status: int = Query(default=None, description="状态码"),
+        username: str = Query(default="", description="用户名称"),
+        request_tags: str = Query(default="", description="请求模块"),
+        request_summary: str = Query(default="", description="请求接口"),
+        request_method: str = Query(default="", description="请求方式"),
+        response_code: int = Query(default=None, description="响应代码"),
         start_time: str = Query(default="", description="开始时间"),
         end_time: str = Query(default="", description="结束时间"),
 ):
     q = Q()
     if username:
         q &= Q(username__icontains=username)
-    if module:
-        q &= Q(module__icontains=module)
-    if method:
-        q &= Q(method__icontains=method)
-    if summary:
-        q &= Q(summary__icontains=summary)
-    if status:
-        q &= Q(status__icontains=status)
+    if request_tags:
+        q &= Q(request_tags__icontains=request_tags)
+    if request_summary:
+        q &= Q(request_summary__icontains=request_summary)
+    if request_method:
+        q &= Q(request_method__icontains=request_method)
+    if response_code:
+        q &= Q(response_code__icontains=response_code)
     if start_time and end_time:
         q &= Q(created_time__range=[start_time, end_time])
     elif start_time:
