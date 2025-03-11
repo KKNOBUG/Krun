6d35ef4d4234de06a2f4e4a72c7f76df23c52aa8
KKNOBUG <807440781@qq.com>
2025-03-11 08:54:36 +0800

    1.新增任务中心目录

---
+++ frontend/src/views/task/list/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppPage>
+    <h3>任务清单</h3>
+  </AppPage>
+</template>
+<script setup>
+import {ref} from 'vue'
+import AppPage from "@/components/page/AppPage.vue";
+import api from "@/api";
+</script>
