4e0df8796896f5ca3d350e09adaa4d56ac27d03e
KKNOBUG <807440781@qq.com>
2025-03-11 08:53:44 +0800

    1.新增自动化测试目录

---
+++ frontend/src/views/autotest/report/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppPage>
+    <h3>测试报告</h3>
+  </AppPage>
+</template>
+<script setup>
+import {ref} from 'vue'
+import AppPage from "@/components/page/AppPage.vue";
+import api from "@/api";
+</script>
---
+++ frontend/src/views/autotest/testcase/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppPage>
+    <h3>测试案例</h3>
+  </AppPage>
+</template>
+<script setup>
+import {ref} from 'vue'
+import AppPage from "@/components/page/AppPage.vue";
+import api from "@/api";
+</script>
