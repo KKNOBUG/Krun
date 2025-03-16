0266bfd85c2dcbd610f6c0bad88d1cf1c4446bb3
KKNOBUG <807440781@qq.com>
2025-03-16 15:43:27 +0800

    1.扩展model脚手架功能

--- backend/applications/base/services/scaffold.py
+++ backend/applications/base/services/scaffold.py
@@ -22,29 +22,61 @@ from backend import GLOBAL_CONFIG
 class ScaffoldModel(models.Model):
     id = fields.BigIntField(pk=True, description="主键")
 
-    async def to_dict(self, m2m: bool = False, exclude_fields: Optional[List[str]] = None):
-        if exclude_fields is None:
-            exclude_fields = []
+    async def to_dict(self,
+                      include_fields: Optional[List[str]] = None,
+                      exclude_fields: Optional[List[str]] = None,
+                      m2m: bool = False,
+                      m2m_include_fields: Optional[List[str]] = None,
+                      m2m_exclude_fields: Optional[List[str]] = None,
+                      fk: bool = False,
+                      fk_include_fields: Optional[List[str]] = None,
+                      fk_exclude_fields: Optional[List[str]] = None,
+                      ):
+        """
+        将模型实例转换为字典形式，支持灵活配置要包含或排除的字段，以及是否处理多对多关系和外键关系。
+        :param include_fields: 需要引入的本表字段列表，默认为 None
+        :param exclude_fields: 需要排除的本表字段列表，默认为 None
+        :param m2m: 是否获取多对多关系字段的数据，默认为 False
+        :param m2m_include_fields: 需要引入的多对多表字段列表，默认为 None
+        :param m2m_exclude_fields: 需要排除的多对多表字段列表，默认为 None
+        :param fk: 是否获取外键字段对应的数据，默认为 False
+        :param fk_include_fields: 需要引入的外键表字段列表，默认为 None
+        :param fk_exclude_fields: 需要排除的外键表字段列表，默认为 None
+        :return:
+        """
+        # 若未提供排除字段列表，则初始化为空列表
+        exclude_fields = exclude_fields or []
+        m2m_exclude_fields = m2m_exclude_fields or []
+        fk_exclude_fields = fk_exclude_fields or []
 
         d = {}
-        for field in self._meta.db_fields:
-            if field not in exclude_fields:
-                value = getattr(self, field)
-                if isinstance(value, date):
-                    value = value.strftime(GLOBAL_CONFIG.DATE_FORMAT)
-                elif isinstance(value, time):
-                    value = value.strftime(GLOBAL_CONFIG.TIME_FORMAT)
-                elif isinstance(value, datetime):
-                    value = value.strftime(GLOBAL_CONFIG.DATETIME_FORMAT)
-                elif isinstance(value, bytes):
-                    value = value.decode("utf-8")
-                elif isinstance(value, Decimal):
-                    value = float(value)
-                d[field] = value
+        # 获取本表字段并根据 include_fields 参数确定要处理的字段列表
+        db_fields_process = self._meta.db_fields
+        if include_fields:
+            db_fields_process = include_fields
 
+        # 遍历字段列表，将字段值添加到字典中，并对特定类型的值进行预处理
+        for field in db_fields_process:
+            if field in exclude_fields:
+                continue
+            value = getattr(self, field)
+            d[field] = await self.__format_value(value)
+
+        # 如果 fk 为 True，异步获取外键字段关联的数据
+        if fk:
+            tasks = [
+                self.__fetch_fk_field(field, fk_include_fields, fk_exclude_fields)
+                for field in self._meta.fk_fields
+                if field not in exclude_fields
+            ]
+            results = await asyncio.gather(*tasks)
+            for field, values in results:
+                d[field] = values
+
+        # 如果 m2m 为 True，异步获取多对多关系字段的数据
         if m2m:
             tasks = [
-                self.__fetch_m2m_field(field, exclude_fields)
+                self.__fetch_m2m_field(field, m2m_include_fields, m2m_exclude_fields)
                 for field in self._meta.m2m_fields
                 if field not in exclude_fields
             ]
@@ -54,25 +86,62 @@ class ScaffoldModel(models.Model):
 
         return d
 
-    async def __fetch_m2m_field(self, field, exclude_fields):
-        values = await getattr(self, field).all().values()
-        formatted_values = []
+    @classmethod
+    async def __format_value(cls, value: Any):
+        if isinstance(value, datetime):
+            value = value.strftime(GLOBAL_CONFIG.DATETIME_FORMAT)
+        elif isinstance(value, date):
+            value = value.strftime(GLOBAL_CONFIG.DATE_FORMAT)
+        elif isinstance(value, time):
+            value = value.strftime(GLOBAL_CONFIG.TIME_FORMAT)
+        elif isinstance(value, bytes):
+            value = value.decode("utf-8")
+        elif isinstance(value, Decimal):
+            value = float(value)
 
-        for value in values:
-            formatted_value = {}
-            for k, v in value.items():
-                if k not in exclude_fields:
-                    if isinstance(v, datetime):
-                        formatted_value[k] = v.strftime(GLOBAL_CONFIG.DATETIME_FORMAT)
-                    else:
-                        formatted_value[k] = v
-            formatted_values.append(formatted_value)
+        return value
 
-        return field, formatted_values
+    async def __fetch_fk_field(self, field, fk_include_fields, fk_exclude_fields):
+        """
+        获取外键字段关联对象的数据，并将其转换为字典形式
+        :param field: 外键字段名
+        :param fk_include_fields: 需要引入的外键表字段列表
+        :param fk_exclude_fields: 需要排除的外键表字段列表
+        :return: 外键字段名和关联对象转换后的字典
+        """
+        fk_instance = getattr(self, field)
+        if fk_instance:
+            return field, await fk_instance.to_dict(
+                include_fields=fk_include_fields,
+                exclude_fields=fk_exclude_fields,
+                m2m=False,  # 避免无限递归
+                fk=False  # 根据需求调整
+            )
+        return field, None
+
+    async def __fetch_m2m_field(self, field, m2m_include_fields, m2m_exclude_fields):
+        """
+        获取多对多关系字段的数据，并将其转换为字典形式
+        :param field: 外键字段名
+        :param m2m_include_fields: 需要引入的外键表字段列表
+        :param m2m_exclude_fields: 需要排除的外键表字段列表
+        :return: 多对多字段名和关联对象转换后的字典
+        """
+        instances = await getattr(self, field).all()
+        values = []
+        for instance in instances:
+            value = await instance.to_dict(
+                include_fields=m2m_include_fields,
+                exclude_fields=m2m_exclude_fields,
+                m2m=False,  # 避免无限递归
+                fk=False  # 根据需求调整
+            )
+            values.append(value)
+        return field, values
 
     class Meta:
-        abstract = True
-        default_connection = "default"
+        abstract = True  # 表明这个类是一个抽象基类，不会在数据库中创建对应的表。其他模型类可以继承这个基类，复用其方法和字段
+        default_connection = "default"  # 指定默认使用的数据库连接
 
 
 class UUIDModel:
@@ -153,17 +222,20 @@ class ScaffoldCrud(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         return await self.model.filter(**kwargs).all()
 
     async def list(self, page: int, page_size: int, search: Q = Q(),
-                   order: Optional[list] = None) -> Tuple[int, List[ModelType]]:
+                   order: Optional[list] = None, related: Optional[list] = None) -> Tuple[int, List[ModelType]]:
         """
         :param page: 页码，从 1 开始。
         :param page_size: 每页的对象数量。
         :param search: 搜索条件，使用 tortoise.expressions.Q 对象。默认为 Q()，表示不进行额外搜索。
         :param order: 排序条件，为一个列表，列表元素为排序字段，默认为空列表，表示不进行排序。
+        :param related: 关联字段，为一个列表，所有的外键字段所对应的信息，默认为None，表示没有关联字段或不查询关联字段。
         :return: 一个元组，包含总对象数和该页的对象列表。
         """
         order: list = order or []
+        related: list = related or []
         query = self.model.filter(search)
-        return await query.count(), await query.offset((page - 1) * page_size).limit(page_size).order_by(*order)
+        return await query.count(), await query.offset((page - 1) * page_size).limit(page_size).order_by(
+            *order).prefetch_related(*related)
 
     async def create(self, obj_in: CreateSchemaType) -> ModelType:
         """
