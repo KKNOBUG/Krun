0b06a424990c53602361a29611f169422aad2c02
KKNOBUG <807440781@qq.com>
2025-03-25 22:59:46 +0800

    1.新增自动化测试-UI测试

---
+++ frontend/src/views/autotest/ui/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <AppPage>
+    <h3>Web UI 测试</h3>
+  </AppPage>
+</template>
+<script setup>
+import {ref} from 'vue'
+import AppPage from "@/components/page/AppPage.vue";
+import api from "@/api";
+</script>
