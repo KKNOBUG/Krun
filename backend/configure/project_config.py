970a1da50a1ebf5d80bdff9b34d06a5f51696fac
KKNOBUG <807440781@qq.com>
2025-03-26 22:58:45 +0800

    1.配置常用的用户代理

--- backend/configure/project_config.py
+++ backend/configure/project_config.py
@@ -152,7 +152,7 @@ class ProjectConfig(BaseSettings):
         return models
 
     # 常用的用户代理字符串列表
-    USER_AGENTS = [
+    USER_AGENTS: List[str] = [
         # Chrome
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
